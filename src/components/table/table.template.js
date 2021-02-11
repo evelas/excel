1fba371a79e55303dedb2f0c04fbdba679332fe3
Evelas <evelas94@gmail.com>
2021-02-11 12:04:28 +0300

    refactor: map(parameter)-> reference

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -10,13 +10,7 @@ const CODES = {
 export function createTable(rowsCount = 10) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
-  const cols = new Array(colsCount)
-    .fill('')
-    .map((el, index) => {
-      return String.fromCharCode(CODES.A + index);
-    })
-    .map((el) => createColumn(el))
-    .join('');
+  const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
   // формируем шапку
   rows.push(createRow(cols));
 
@@ -36,8 +30,15 @@ function createRow(content) {
   `;
 }
 
-function createColumn(col) {
+function toColumn(col) {
   return `
     <div class="column">${col}</div>
   `;
+}
+
+// _ - placeholder, нам нужно его обозначить
+// чтобы получить доступ до второго
+// входящего значения - index
+function toChar(_, index) {
+  return String.fromCharCode(CODES.A + index);
 }
