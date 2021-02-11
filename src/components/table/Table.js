df268abcc84135da48c931af600c6d3f4be83160
Evelas <evelas94@gmail.com>
2021-02-11 13:54:50 +0300

    finish base framework func

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -5,36 +5,6 @@ export class Table extends ExcelComponent {
   static className = 'excel__table';
 
   toHTML() {
-    return createTable();
-    // return `
-    //       <div class="row">
-    //         <div class="row-info"></div>
-
-    //         <div class="row-data">
-    //           <div class="column">A</div>
-    //           <div class="column">B</div>
-    //           <div class="column">C</div>
-    //         </div>
-    //       </div>
-
-    //       <div class="row">
-    //         <div class="row-info">1</div>
-
-    //         <div class="row-data">
-    //           <div class="cell selected" contenteditable>a1</div>
-    //           <div class="cell" contenteditable>b2</div>
-    //           <div class="cell" contenteditable>c2</div>
-    //         </div>
-    //       </div>
-    //       <div class="row">
-    //         <div class="row-info">2</div>
-
-    //         <div class="row-data">
-    //           <div class="cell" contenteditable>a1</div>
-    //           <div class="cell" contenteditable>b2</div>
-    //           <div class="cell" contenteditable>c2</div>
-    //         </div>
-    //       </div>
-    // `;
+    return createTable(5);
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,7 +1,6 @@
 // Pure func
-// первый row немного отличается названиями колонок
-
 // String.fromCharCode(65) = 'A'
+
 const CODES = {
   A: 65,
   Z: 90,
@@ -10,21 +9,28 @@ const CODES = {
 export function createTable(rowsCount = 10) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
-  const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
-  // формируем шапку
-  rows.push(createRow(cols));
-
+  const cols = generateColumns(colsCount);
+  // формируем ряд с шапкой
+  rows.push(createRow('', cols));
+  // формируем остальные ряды
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow());
+    rows.push(createRow(i + 1, generateCells(colsCount)));
   }
-
   return rows.join('');
 }
 
-function createRow(content) {
+function generateColumns(colsCount) {
+  return new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
+}
+
+function generateCells(colsCount) {
+  return new Array(colsCount).fill('').map(toCell).join('');
+}
+
+function createRow(rowIndex, content) {
   return `
     <div class="row">
-      <div class="row-info"></div>
+      <div class="row-info">${rowIndex}</div>
       <div class="row-data">${content}</div>
     </div>
   `;
@@ -33,6 +39,12 @@ function createRow(content) {
 function toColumn(col) {
   return `
     <div class="column">${col}</div>
+  `;
+}
+
+function toCell(content) {
+  return `
+    <div class="cell" contenteditable>${content}</div>
   `;
 }
 
