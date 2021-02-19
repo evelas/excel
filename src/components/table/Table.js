ec43fe269086706f49f05779617d33dac5e0b105
Evelas <evelas94@gmail.com>
2021-02-19 11:07:18 +0300

    some func for resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -21,22 +21,27 @@ export class Table extends ExcelComponent {
       // const $parent = $resizer.$el.parentNode; // Если добавить еще блок например с фильтром то родитель будет другим // Bad
       // const $parent = $resizer.$el.closest(); // Bad так как могут изменится классы
       const $parent = $resizer.closest('[data-type="resizable"]');
+
       const coords = $parent.getCoordsNode();
       const whichColumn = $parent.data.col;
       const column = this.$root.findAll(`[data-col="${whichColumn}"]`);
+      const type = $resizer.data.resize;
+      console.log(type);
       document.onmousemove = (e) => {
         // coords.right статическое значение
         // e.pageX динамическое координаты меняются при движении мышки
         // Delta - растояние от куда мы передвинули мышку
         // до правого край (так как ресайз на правом крае)
-        if (whichColumn) {
+        if (type === 'col') {
           const deltaX = e.pageX - coords.right;
           const valueX = coords.width + deltaX;
           column.forEach((el) => (el.style.width = valueX + 'px'));
         } else {
           const deltaY = e.pageY - coords.bottom;
           const valueY = coords.height + deltaY;
-          $parent.$el.style.height = valueY + 'px';
+          console.log(valueY);
+          // $parent.$el.style.height = valueY + 'px';
+          $parent.css({ height: valueY + 'px' });
         }
       };
 
--- src/core/dom.js
+++ src/core/dom.js
@@ -59,6 +59,13 @@ class Dom {
   findAll(selector) {
     return this.$el.querySelectorAll(selector);
   }
+
+  css(styles = {}) {
+    Object.keys(styles).forEach((key) => {
+      // style[key]
+      return (this.$el.style[key] = styles[key]);
+    });
+  }
 }
 export function $(selector) {
   return new Dom(selector);
