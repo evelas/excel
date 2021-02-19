47b56834c2053bca6a361a3106bc95a5c7802caa
Evelas <evelas94@gmail.com>
2021-02-19 12:01:57 +0300

    refactor: resize only on mouseUp

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,7 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from './table.template';
-import { $ } from '@core/dom';
-
+import { resizeHandler } from './table.resize';
+import { shouldResize } from './table.helpers';
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
@@ -12,42 +12,12 @@ export class Table extends ExcelComponent {
   }
 
   toHTML() {
-    return createTable(35);
+    return createTable(25);
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target);
-      // const $parent = $resizer.$el.parentNode; // Если добавить еще блок например с фильтром то родитель будет другим // Bad
-      // const $parent = $resizer.$el.closest(); // Bad так как могут изменится классы
-      const $parent = $resizer.closest('[data-type="resizable"]');
-
-      const coords = $parent.getCoordsNode();
-      const whichColumn = $parent.data.col;
-      const column = this.$root.findAll(`[data-col="${whichColumn}"]`);
-      const type = $resizer.data.resize;
-      console.log(type);
-      document.onmousemove = (e) => {
-        // coords.right статическое значение
-        // e.pageX динамическое координаты меняются при движении мышки
-        // Delta - растояние от куда мы передвинули мышку
-        // до правого край (так как ресайз на правом крае)
-        if (type === 'col') {
-          const deltaX = e.pageX - coords.right;
-          const valueX = coords.width + deltaX;
-          column.forEach((el) => (el.style.width = valueX + 'px'));
-        } else {
-          const deltaY = e.pageY - coords.bottom;
-          const valueY = coords.height + deltaY;
-          console.log(valueY);
-          // $parent.$el.style.height = valueY + 'px';
-          $parent.css({ height: valueY + 'px' });
-        }
-      };
-
-      document.onmouseup = (e) => {
-        document.onmousemove = null;
-      };
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
     }
   }
 }
