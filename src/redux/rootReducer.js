3100fc27b4289189f97ced870674e8907c5ae9da
Evelas <evelas94@gmail.com>
2021-03-01 17:40:49 +0300

    refactor rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,21 +2,23 @@
 import { CHANGE_TEXT, TABLE_RESIZE } from './actions/types';
 
 export function rootReducer(state, action) {
-  let prevState;
   let field;
   // console.log('Action:', action);
   switch (action.type) {
     case TABLE_RESIZE:
       field = action.data.type === 'col' ? 'colState' : 'rowState';
-      prevState = state[field] || {};
-      prevState[action.data.id] = action.data.value;
       // динамическое значение field обернем []
-      return { ...state, [field]: prevState };
+      return { ...state, [field]: value(state, field, action) };
     case CHANGE_TEXT:
-      prevState = state['dataState'] || {};
-      prevState[action.data.id] = action.data.value;
-      return { ...state, currentText: action.data.value, dataState: prevState };
+      field = 'dataState';
+      return { ...state, currentText: action.data.value, field: value(state, field, action) };
     default:
       return state;
   }
+}
+
+function value(state, field, action) {
+  const val = state[field] || {};
+  val[action.data.id] = action.data.value;
+  return val;
 }
